a2ae26f075c69a75319b74b6ed9be188d2814263
Marios Patsis <mariospatsis@gmail.com>
2023-11-03 16:43:22 +0200

    auth UI

--- frontend/src/auth/login/Login.js
+++ frontend/src/auth/login/Login.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import styles from './Login.module.css';
 import { Input, Button } from '@chakra-ui/react'
 
 
 export const Login = () => {
+    const [username, setUsername] = useState('');
+
+
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.head}>
@@ -10,8 +15,8 @@ export const Login = () => {
                 <div>Type your username to continue</div>
             </div>
             <div className={styles.form}>
-                <Input placeholder='Username' size='lg' />
-                <Button colorScheme='teal' size='lg'>
+                <Input placeholder='Username' size='lg' value={username} onChange={(e)=> setUsername(e.target.value)} />
+                <Button colorScheme='teal' size='lg' isDisabled={!username}>
                     login
                 </Button>
             </div>
--- frontend/src/auth/signup/Singup.js
+++ frontend/src/auth/signup/Singup.js
@@ -1,18 +1,54 @@
+import { useState } from 'react';
 import styles from './Signup.module.css';
-import { Input, Button } from '@chakra-ui/react';
-
+import { Input, Button, Select } from '@chakra-ui/react';
+import { SingleDatepicker } from "chakra-dayzed-datepicker";
 
 export const Signup = () => {
+    const [form, setForm] = useState({
+        username: '',
+        firstname: '',
+        lastname: '',
+        birthday: new Date(),
+        gender: 'N/A',
+    });
+    const isFormValid = Object.values(form).every(Boolean);
+
+    const onChangeInputHandler = (e) =>{
+        e['target']?
+            setForm(prevState=> ({
+                ...prevState,
+                [e.target.name]: e.target.value
+            })):
+            setForm(prevState=> ({
+                ...prevState,
+                birthday: e,
+            }));
+    }
+
     return (
         <div className={styles.wrapper}>
             <div className={styles.head}>
-                <div className={styles.title}>Signup</div>
+                <div className={styles.title}>Sign up</div>
                 <div>Register if you don't have a username</div>
             </div>
             <div className={styles.form}>
-                <Input placeholder='Username' size='lg' />
-                <Button colorScheme='teal' size='lg'>
-                    login
+                <Input name='username' placeholder='Username' size='lg' value={form['username']} onChange={onChangeInputHandler}/>
+                <Input name='firstname' placeholder='Firstname' size='lg' value={form['firstname']} onChange={onChangeInputHandler} />
+                <Input name='lastname' placeholder='Lastname' size='lg' value={form['lastname']} onChange={onChangeInputHandler} />
+                <SingleDatepicker
+                    name='birthday'
+                    configs={configs}
+                    date={form['birthday']}
+                    onDateChange={onChangeInputHandler}
+                />
+                <Select name='gender' size='lg' value={form['gender']} onChange={onChangeInputHandler}>
+                    <option value='N/A'>N/A</option>
+                    <option value='Male'>Male</option>
+                    <option value='Female'>Female</option>
+                </Select>
+                
+                <Button colorScheme='teal' size='lg' isDisabled={!isFormValid}>
+                    Sign up
                 </Button>
             </div>
         </div>
